2d0216be9607586b1e1b79ed150a8b22c915d6a2
Thomas Fire <tomasfire@live.com>
2021-03-08 10:37:27 +0300

    Added clang result
    Signed-off-by: thomasfire <tomasfire@live.com>

--- lab_1/build.sh
+++ lab_1/build.sh
@@ -5,3 +5,6 @@ gcc -O3 -Wall -Werror -o lab1_par_2  main.c -lm -floop-parallelize-all -ftree-pa
 gcc -O3 -Wall -Werror -o lab1_par_3  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=3
 gcc -O3 -Wall -Werror -o lab1_par_4  main.c -lm -floop-parallelize-all -ftree-parallelize-loops=4
 gcc -O3 -Wall -Werror -o lab1_seq  main.c -lm
+
+clang -O3 -Wall -Werror -o lab1_seq_clang  main.c -lm
+clang -O3 -Wall -Werror -o lab1_par_clang  main.c -lm -ftree-slp-vectorize
