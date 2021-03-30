a138edda1130abaf683f8d712b7825d65a6b9cad
Thomas Fire <tomasfire@live.com>
2021-03-31 01:32:15 +0300

    Using TBB
    Signed-off-by: thomasfire <tomasfire@live.com>

--- lab_2/build.sh
+++ lab_2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-icx -O3 -Wall -Werror -o lab_2_clang  main.c -I $IPPROOT/include -L $IPPROOT/lib/intel64 -lippi -lipps -lippcore -lippvm
-icc -O3 -Wall -Werror -o lab_2_intel  main.c -I $IPPROOT/include -L $IPPROOT/lib/intel64 -lippi -lipps -lippcore -lippvm
+icx -O3 -Wall -Werror -o lab_2_clang  main.c -I $IPPROOT/include -L $IPPROOT/lib/intel64/tl/tbb/ -lippi_tl_tbb -lipps -lippcore_tl_tbb -lippvm -lippcore -lm
+icc -O3 -Wall -Werror -o lab_2_intel  main.c -I $IPPROOT/include -L $IPPROOT/lib/intel64/tl/tbb/ -lippi_tl_tbb -lipps -lippcore_tl_tbb -lippvm -lippcore -lm
